1afc00f3f26472083d814a99f244dae39a960e34
Pacien TRAN-GIRARD <pacien.trangirard@pacien.net>
2017-07-11 16:37:08 +0200

    Fix hosts scripts deletion

--- app/src/main/java/org/pacien/tincapp/commands/TincApp.kt
+++ app/src/main/java/org/pacien/tincapp/commands/TincApp.kt
@@ -14,7 +14,7 @@ object TincApp {
     private val STATIC_SCRIPTS = listOf("tinc", "host", "subnet", "invitation").flatMap { s -> SCRIPT_SUFFIXES.map { s + it } }
 
     private fun listScripts(netName: String) = AppPaths.confDir(netName).listFiles { f -> f.name in STATIC_SCRIPTS } +
-            AppPaths.hostsDir(netName).listFiles { f -> SCRIPT_SUFFIXES.none { f.name.endsWith(it) } }
+            AppPaths.hostsDir(netName).listFiles { f -> SCRIPT_SUFFIXES.any { f.name.endsWith(it) } }
 
     fun removeScripts(netName: String): CompletableFuture<Void> = runAsyncTask {
         listScripts(netName).forEach { it.delete() }
