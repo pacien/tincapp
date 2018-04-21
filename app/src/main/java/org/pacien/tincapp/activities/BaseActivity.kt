89f8f3d1b7099da47330b869eb2f8038aa152e56
pacien <pacien.trangirard@pacien.net>
2018-04-21 19:56:44 +0200

    Simplify action bar

--- app/src/main/java/org/pacien/tincapp/activities/BaseActivity.kt
+++ app/src/main/java/org/pacien/tincapp/activities/BaseActivity.kt
@@ -23,7 +23,6 @@ abstract class BaseActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.base)
-    setSupportActionBar(toolbar)
     handleRecentCrash()
   }
 
