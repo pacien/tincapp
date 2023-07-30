08e009f62d352030860b6931f2d393d07bca1c6e
pacien <pacien.trangirard@pacien.net>
2023-07-30 03:19:08 +0200

    screen/configuration: fix app crash on fast tap on tools

--- app/src/main/java/org/pacien/tincapp/activities/configure/ToolsFragment.kt
+++ app/src/main/java/org/pacien/tincapp/activities/configure/ToolsFragment.kt
@@ -1,6 +1,6 @@
 /*
  * Tinc App, an Android binding and user interface for the tinc mesh VPN daemon
- * Copyright (C) 2017-2020 Pacien TRAN-GIRARD
+ * Copyright (C) 2017-2023 Pacien TRAN-GIRARD
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -46,5 +46,5 @@ class ToolsFragment : BaseFragment() {
   }
 
   private fun openDialog(tool: ConfigurationToolDialogFragment): () -> Unit =
-    { tool.show(fragmentManager!!, tool.javaClass.simpleName) }
+    { if (!tool.isAdded) tool.show(fragmentManager!!, tool.javaClass.simpleName) }
 }
